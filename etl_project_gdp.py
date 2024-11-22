1f851ab83ee0143fdef74a75d3b2ea4af8f993fa
H3VO <hevonox@gmail.com>
2024-11-22 17:17:45 -0600

    creamos la estructura base del proyecto

---
+++ etl_project_gdp.py
@@ -0,0 +1,49 @@
+# Code for ETL operations on Country-GDP data
+
+# Importing the required libraries
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs= ['Country', 'GDP_USD_millions']
+db_name= 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path ='Countries_by_GDP.csv'
+
+def extract(url, table_attribs):
+    ''' This function extracts the required
+    information from the website and saves it to a dataframe. The
+    function returns the dataframe for further processing. '''
+
+    return df
+
+def transform(df):
+    ''' This function converts the GDP information from Currency
+    format to float value, transforms the information of GDP from
+    USD (Millions) to USD (Billions) rounding to 2 decimal places.
+    The function returns the transformed dataframe.'''
+
+    return df
+
+def load_to_csv(df, csv_path):
+    ''' This function saves the final dataframe as a `CSV` file 
+    in the provided path. Function returns nothing.'''
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final dataframe as a database table
+    with the provided name. Function returns nothing.'''
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+def log_progress(message):
+    ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
+
+''' Here, you define the required entities and call the relevant 
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
