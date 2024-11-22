cc293ebeefbe97374a9b6e6ff6e6d243172fbcd3
H3VO <hevonox@gmail.com>
2024-11-22 17:56:32 -0600

    creamos la funcion que extraera los datos de las fuentes

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -18,6 +18,20 @@ def extract(url, table_attribs):
     ''' This function extracts the required
     information from the website and saves it to a dataframe. The
     function returns the dataframe for further processing. '''
+    html_page = requests.get(url).text
+    data = BeautifulSoup(html_page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows= tables[2].find_all('tr')
+
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                             "GDP_USD_millions": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
 
     return df
 
@@ -47,3 +61,4 @@ def log_progress(message):
 ''' Here, you define the required entities and call the relevant 
 functions in the correct order to complete the project. Note that this
 portion is not inside any function.'''
+
