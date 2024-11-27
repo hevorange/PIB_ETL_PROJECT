039ff3b5a203f97501ab7e11ff7097d9d9f4acdb
H3VO <hevonox@gmail.com>
2024-11-26 23:03:50 -0600

    Terminamos todo el funcionamiento del codigo y creamos el archivo .gitignore para excluir los archivos que no son necesarios, ya que el propio codigo creará esos archivos al momento de ser ejecutado

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -80,3 +80,31 @@ def log_progress(message):
 functions in the correct order to complete the project. Note that this
 portion is not inside any function.'''
 
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('World_Economies.db')
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as table. Running the query')
+
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
